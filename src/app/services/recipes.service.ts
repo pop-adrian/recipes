a91766a7bb17a648a65e4e28f47784fa9b2cddff
Oprescu Stefan <ioprescu14@yahoo.com>
2020-06-04 18:13:42 +0300

    Add use-ingredient-service

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -2,18 +2,29 @@ import { Injectable } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
 import {Ingredient} from '../models/ingredient.model';
 import { RecipeIngredient } from '../models/recipe-ingredient.model';
+import { IngredientsService } from './ingredients.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
 
-  constructor() { }
+  constructor(private ingredientsService :  IngredientsService) { }
 
   getRecipes()  : Recipe[]{
-    let ing : Ingredient = {id: 1, name: 'flour'};
-    let recipeIngredient : RecipeIngredient = {id:1 , ingredient : ing, quantity: 2};
-    let recipeIngredients : RecipeIngredient[] = [recipeIngredient];
+    let ingredients : Array<Ingredient> = this.ingredientsService.getIngredients();
+    let recipeIngredients : RecipeIngredient[] = new Array();
+    let currentIndex=-1;
+    for (var i = 0 ; i <3 ;i++){
+      var index=Math.floor(Math.random() * ingredients.length)
+      if(currentIndex!=index){
+        var ing : Ingredient = ingredients[index];
+        let recipeIngredient : RecipeIngredient = {id:1 , ingredient : ing, quantity: 2};
+        recipeIngredients.push(recipeIngredient);
+        currentIndex=index;
+        console.log(recipeIngredient);
+      }
+    }
     let r1 : Recipe = {id: 1, name: 'bread', description: 'abc', ingredients: recipeIngredients};
     let r2 : Recipe = {id: 2, name: 'pancakes', description: 'bcd', ingredients: recipeIngredients};
     let r3 : Recipe = {id: 3, name: 'pizza', description: 'cde', ingredients: recipeIngredients};
