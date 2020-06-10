3a426466b5cf8e715717e9b4f4f666fdef358eff
Adrian Pop <44285786+pop-adrian@users.noreply.github.com>
2020-06-10 12:19:47 +0300

    Add Alternative Ingredient

--- api/WebApplication1/WebApplication1/Models/RecipeIngredient.cs
+++ api/WebApplication1/WebApplication1/Models/RecipeIngredient.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,17 @@ namespace Recipes.Models
   public class RecipeIngredient
   {
     public int Id { get; set; }
+
+    [ForeignKey(nameof(Ingredient))]
     public int IngredientId { get; set; }
+
     public Ingredient Ingredient { get; set; }
+
+    [ForeignKey(nameof(AlternativeIngredient))]
+    public int? AlternativeIngredientId { get; set; }
+
+    public Ingredient AlternativeIngredient { get; set; }
+
     public double Quantity { get; set; }
 
     public int RecipeId { get; set; }
